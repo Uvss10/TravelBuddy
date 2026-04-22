29dcf27514e2abb16348f385a363072b5c55b4f2
Hem Singh <hemsinghrathore994@gmail.com>
2026-04-22 14:32:57 +0530

    Migrate to Supabase Cloud architecture (Code Only)

--- mobile/android/app/src/main/kotlin/com/example/travel_buddy/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.travel_buddy
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- mobile/lib/config/app_navigator.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-
-/// App-wide navigator handle for UI elements rendered above Navigator context.
-class AppNavigator {
-  static final GlobalKey<NavigatorState> key = GlobalKey<NavigatorState>();
-}
